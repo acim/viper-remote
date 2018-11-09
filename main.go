cd0157968ecb150732dbaaf49b7f3fc58926b342
Boban Acimovic <boban.acimovic@gmail.com>
2018-11-10 00:01:57 +0100

    add runtime configuration

--- main.go
+++ main.go
@@ -2,22 +2,46 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 
 	"github.com/spf13/viper"
 	_ "github.com/spf13/viper/remote"
 )
 
+type config struct {
+	Port int
+	mux  sync.Mutex
+}
+
 func main() {
-	viper.AddRemoteProvider("consul", "consul:8500", "config/app")
-	viper.SetConfigType("yaml")
+	rv := viper.New()
+	rv.AddRemoteProvider("consul", "consul:8500", "config/app")
+	rv.SetConfigType("yaml")
+	err := rv.ReadRemoteConfig()
+	if err != nil {
+		fmt.Println("Please set key config/app in Consul")
+	}
+
+	c := &config{}
+
+	go func() {
+		for {
+			time.Sleep(time.Second * 5)
+			err := rv.WatchRemoteConfig()
+			if err != nil {
+				fmt.Println("Please set key config/app in Consul")
+				continue
+			}
+
+			c.mux.Lock()
+			rv.Unmarshal(c)
+			c.mux.Unlock()
+		}
+	}()
 
 	for {
-		err := viper.ReadRemoteConfig()
-		if err != nil {
-			fmt.Println("Please set key config/app in Consul with YML ")
-		}
-		fmt.Println(viper.Get("port"))
-		time.Sleep(5 * time.Second)
+		time.Sleep(time.Second * 5)
+		fmt.Printf("%#v\n", c)
 	}
 }
